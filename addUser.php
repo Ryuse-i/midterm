42c321992e7c3f781b7272af24d423f3fd7226ee
D-land06 <mangaoangdylan06@gmail.com>
2025-09-16 11:54:50 +0800

    Add add-user feature

--- addUser.php
+++ addUser.php
@@ -8,17 +8,88 @@
 
     require_once 'db.php';
 
-    if($_SERVER['REQUEST_METHOD'] === 'POST'){
-        
+    if($_SERVER["REQUEST_METHOD"] === "POST"){
+        $name = trim($_POST['name']);
+        $email = trim($_POST['email']);
+        $password = trim($_POST['password']);
+
+        if(empty($name) || empty($email) || empty($password)){
+            header('Location: loginForm.php?user=empty_fields');
+            exit;
+        }
+
+        if(strlen($password) < 8){
+            header('Location: registerForm.php?user=pass_short');
+            exit;
+        }
+
+        if(!preg_match('/[A-Z]/', $password)){
+            header('Location: registerForm.php?user=no_uppercase');
+            exit;
+        }
+
+        if(!preg_match('/[a-z]/', $password)){
+            header('Location: registerForm.php?user=no_lowercase');
+            exit;
+        }
+
+        if(!preg_match('/\d/', $password)){
+            header('Location: registerForm.php?user=no_digits');
+            exit;
+        }
+
+        if(!preg_match('/\W/', $password)){
+            header('Location: registerForm.php?user=no_special_chars');
+            exit;
+        }
+
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            header('Location: registerForm.php?user=invalid_email');
+            exit;
+        }
+
+        $cleanName = filter_var($name, FILTER_SANITIZE_SPECIAL_CHARS);
+        $validEmail = filter_var($email, FILTER_SANITIZE_EMAIL);
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
         try{
-            $sql = 'INSERT INTO users (name, email) VALUES (:name, :email)';
+            $sql = 'SELECT * FROM users WHERE email = :email';
             $statement = $pdo->prepare($sql);
-            $statement->execute(['name' => $name, 'email' => $email]);
+            $statement->bindValue(":email", $validEmail, PDO::PARAM_STR);
+            $statement->execute();
 
-            header('Location: dashboard.php?action=add_success');
+            $user = $statement->fetch(PDO::FETCH_ASSOC);
+        } catch(PDOException $error){
+            die("ERROR: " . $error->getMessage());
+        }
+
+        if(!$user){
+            try{
+                $sql = 'INSERT INTO users (name, email, password) VALUES (:name, :email, :password)';
+                $statement = $pdo->prepare($sql);
+                $statement->bindValue(":name", $cleanName, PDO::PARAM_STR);
+                $statement->bindValue(":email", $validEmail, PDO::PARAM_STR);
+                $statement->bindValue(":password", $hashedPassword, PDO::PARAM_STR);
+                $statement->execute();
+                $row =$statement->rowCount();
+
+                header('Location: dashboard.php');
+                exit;
+            }catch(PDOException $error){
+                header('Location: addUserForm.php?user=add_failed');
+                die("ERROR: " . $error->getMessage());
+            }
+        }
+        else{
+            header('Location: addUserForm.php?user=already_exist');
             exit;
-        }catch(PDOException $error){
-            die("ERROR" . $error->getMessage());
+        }
+
+        if($row){
+            header('Location: dashboard.php?user=add_success');
+            exit;
+        } else {
+            header('Location: addUserForm.php?user=add_failed');
+            exit;
         }
     }
--- addUserForm.php
+++ addUserForm.php
@@ -1,12 +1,73 @@
+<?php 
+   session_start();
+   
+    if(!isset($_SESSION['user'])){
+         header('Location: loginForm.php');
+         exit;
+    }
+
+    $toastMessage = null;
+    $toastType = null;
+    if (isset($_GET['user'])) {
+        switch ($_GET['user']) {
+            case 'empty_fields':
+                $toastMessage = "Please fill out all fields";
+                $toastType = "error";
+                break;
+            case 'pass_short':
+                $toastMessage = "Password must be at least 8 characters long";
+                $toastType = "error";
+                break;      
+            case 'no_uppercase':
+                $toastMessage = "Password must contain at least one uppercase letter";
+                $toastType = "error";
+                break;
+            case 'no_lowercase':    
+                $toastMessage = "Password must contain at least one lowercase letter";
+                $toastType = "error";
+                break;
+            case 'no_digits':
+                $toastMessage = "Password must contain at least one digit";
+                $toastType = "error";
+                break;
+            case 'no_special_chars':
+                $toastMessage = "Password must contain at least one special character";
+                $toastType = "error";
+                break;
+            case 'invalid_email':
+                $toastMessage = "Invalid email address";
+                $toastType = "error";   
+                break;
+            case 'already_exist':
+                $toastMessage = "User with this email already exists";
+                $toastType = "error";   
+                break;
+            case 'add_success':
+                $toastMessage = "User added successfully";
+                $toastType = "success";   
+                break;  
+            case 'add_failed':
+                $toastMessage = "Failed to add user. Please try again.";
+                $toastType = "error";   
+                break;  
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Add User</title>
 </head>
 <body>
-    <h1>Add User</h1>
+    
+    <div id="Form-head">
+        <h1>Add User</h1>
+       <p>Enter user details below to add a new user</p>
+    </div>
     <form action="addUser.php" method="POST">
         <label for="name">Name</label> <br>
         <input type="text" name="name" placeholder="Full Name" required><br><br>
@@ -15,11 +76,25 @@
         <label for="password">Password</label> <br>
         <input type="password" name="password" placeholder="Password" 
         required
+        autocomplete="new-password"
         minlength="8" 
         pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{8,}"
         title="Password must be at least 8 characters and include uppercase, lowercase, number, and special character"
         ><br><br>
         <button type="submit" id="submit-form">Add User</button> 
     </form>
+
+    <div id="display-validation">
+        <p id="display-validation_message">hatdog</p>
+    </div>
+    
+    <script src="function.js"></script>
+    <script>
+        <?php if (isset($toastMessage) && $toastMessage): ?>
+            document.addEventListener("DOMContentLoaded", () => {
+                toasterDisplay("<?= $toastMessage ?>", "<?= $toastType ?>");
+            });
+        <?php endif; ?>
+    </script>
 </body>
 </html>
